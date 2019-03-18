afdf488e2d3a06c2c1c32321005f2b47cccd4ea3
Jon Jagger <jon@jaggersoft.com>
2019-03-18 09:50:15 +0000

    switch to use new starter-base repo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,17 +3,16 @@ set -ev
 
 readonly GITHUB_ORG=https://raw.githubusercontent.com/cyber-dojo
 readonly CREATE_IMAGE_SCRIPT=cyber_dojo_start_points_create.sh
-readonly IMAGE_NAME=cyberdojo/languages
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-languages.XXXXXXXXX)
 
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
 
 cd ${TMP_DIR}
-curl -O "${GITHUB_ORG}/start-points-base/master/${CREATE_IMAGE_SCRIPT}"
+curl -O --silent "${GITHUB_ORG}/starter-base/master/${CREATE_IMAGE_SCRIPT}"
 chmod 700 ./${CREATE_IMAGE_SCRIPT}
 
 ./${CREATE_IMAGE_SCRIPT} \
-    ${IMAGE_NAME} \
-    --languages \
-      "$(curl -O "${GITHUB_ORG}/languages/master/languages_list_all")"
+    cyberdojo/languages \
+      --languages \
+        "$(curl --silent "${GITHUB_ORG}/languages/master/languages_list_all")"
