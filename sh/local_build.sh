84db2def5cb089eb6c46fd8564b41e159aa22fdb
Jon Jagger <jon@jaggersoft.com>
2019-04-16 13:56:48 +0100

    use expanded-paths to avoid ..'s in local_build.sh

--- sh/local_build.sh
+++ sh/local_build.sh
@@ -2,12 +2,13 @@
 set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+readonly CDL_DIR="$(cd "${ROOT_DIR}" && cd ../../cyber-dojo-languages && pwd )"
 readonly SHA_VALUE=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
 SHA="${SHA_VALUE}" \
   ${ROOT_DIR}/../commander/cyber-dojo start-point create \
     cyberdojo/languages-small \
       --languages \
-        file://${ROOT_DIR}/../../cyber-dojo-languages/gcc-assert      \
-        file://${ROOT_DIR}/../../cyber-dojo-languages/python-unittest \
-        file://${ROOT_DIR}/../../cyber-dojo-languages/ruby-minitest
+        file://${CDL_DIR}/gcc-assert      \
+        file://${CDL_DIR}/python-unittest \
+        file://${CDL_DIR}/ruby-minitest
